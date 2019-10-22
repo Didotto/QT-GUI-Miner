bb3c14908ef2dc1571e569d87feda342bc6018d7
Davide Balice <basket-one@libero.it>
2019-10-22 18:27:53 +0200

    Main View Setup

--- apps/QT-GUI-Client/src/main/view/MainView.java
+++ apps/QT-GUI-Client/src/main/view/MainView.java
@@ -1,6 +1,10 @@
 package view;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
 import javafx.stage.Stage;
 
 public class MainView extends Application {
@@ -10,9 +14,21 @@ public class MainView extends Application {
 	}
 	
 	@Override
-	public void start(Stage stage) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("prova.fxml")); 
+        Scene scene = new Scene(root, 550, 500);
+        primaryStage.setTitle("QT-Miner JavaFX");
+        
+        //Disable Split Pane divider
+        SplitPane splitPane = (SplitPane)scene.lookup("#splitPane");
+        splitPane.lookupAll(".split-pane-divider").stream()
+    		.forEach(div ->  div.setMouseTransparent(true) );
+        
+        primaryStage.setMinHeight(500);
+        primaryStage.setMinWidth(500);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show(); 
 	}
 
 }
