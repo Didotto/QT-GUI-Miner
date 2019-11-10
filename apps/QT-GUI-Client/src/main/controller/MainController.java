f67551b4962b6988d81a9e9bd24a237799976061
Davide Balice <basket-one@libero.it>
2019-11-10 19:12:58 +0100

    Connection Options Window added

--- apps/QT-GUI-Client/src/main/controller/MainController.java
+++ apps/QT-GUI-Client/src/main/controller/MainController.java
@@ -6,6 +6,15 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.ComboBox;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
 
 public class MainController{
 	@FXML
@@ -20,6 +29,10 @@ public class MainController{
 	@FXML
 	private AnchorPane rightPane;
 	
+	@FXML
+	private ImageView gearIcon;
+	
+	@FXML
 	public void initialize() {
 		loadOptions.setItems(FXCollections.observableArrayList(
 				new String("Load From Database"),
@@ -38,6 +51,21 @@ public class MainController{
 			//TO-DO ask the server for file names
 			rightPane.setDisable(false);
 			leftPane.setDisable(true);
+		}
+	}
+	
+	public void gearIconCliecked(MouseEvent event) {
+		AnchorPane root;
+		try {
+			//probabilmente va definito in una nuova classe View
+			root = FXMLLoader.load(getClass().getResource("../view/connectionOptions.fxml"));
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root, root.getPrefWidth(), root.getPrefHeight()));
+			stage.show();
+		} catch(IOException e) {
+			System.out.println("Errore " + e.getMessage());
+		} catch(Exception e ) {
+			System.out.println("Errore " + e.getMessage());
 		}
 	}
 }
