c6f486f6a4798cc634a491382048f9a172ae4f59
Davide Balice <basket-one@libero.it>
2019-11-12 18:57:58 +0100

    Added attributes about TextFields

--- apps/QT-GUI-Client/src/main/model/DataModel.java
+++ apps/QT-GUI-Client/src/main/model/DataModel.java
@@ -3,6 +3,8 @@ package model;
 import java.net.Socket;
 import java.net.InetSocketAddress;
 import java.net.InetAddress;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 import java.net.UnknownHostException;
 import java.io.IOException;
@@ -11,6 +13,16 @@ import java.util.Observable;
 public class DataModel extends Observable{
 	private Socket mySocket;
 	
+	private ObjectInputStream input;
+	
+	private ObjectOutputStream output;
+	
+	private String tableName;
+	
+	private Double radius;
+	
+	private String fileName;
+	
 	public DataModel() {
 		mySocket = new Socket();
 	}
@@ -18,11 +30,45 @@ public class DataModel extends Observable{
 	public void connect(String ipAddress, int port) throws IOException, UnknownHostException {
 		InetSocketAddress endpoint = new InetSocketAddress(InetAddress.getByName(ipAddress), port);
 		mySocket.connect(endpoint);
+		output = new ObjectOutputStream(mySocket.getOutputStream());
+		input = new ObjectInputStream(mySocket.getInputStream());
 		setChanged();
 		notifyObservers();
 	}
 	
 	public boolean isConnected() {
 		return mySocket.isConnected();
+	}
+	
+	public ObjectInputStream getInputStream() throws IOException{
+		return input;
+	}
+	
+	public ObjectOutputStream getOutputStream() throws IOException{
+		return output;
+	}
+	
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	
+	public void setRadius(Double radius) {
+		this.radius = radius;
+	}
+	
+	public String getFileName() {
+		return fileName;
+	}
+	
+	public String getTableName() {
+		return tableName;
+	}
+	
+	public Double getRadius() {
+		return radius;
 	}
 }
