f94b397da475754b97043c89d05ecb2fd999ad8c
Samuele del Vescovo <delvescovo.crafter@gmail.com>
2019-11-13 23:08:52 +0100

    Added SaveController.java (da finire)

---
+++ apps/QT-GUI-Client/src/main/controller/SaveController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SaveController extends Controller{
+	@FXML
+	private Button saveButton;
+	
+	@FXML
+	private Button resetButton;
+	
+	@FXML
+	private TextField fileName;
+}
