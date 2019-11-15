543af566fe40058a19bc267a58180d2087d1b539
Samuele del Vescovo <delvescovo.crafter@gmail.com>
2019-11-15 13:21:01 +0100

    Added Overriding init for Connect or Disconnect

--- apps/QT-GUI-Client/src/main/controller/GearController.java
+++ apps/QT-GUI-Client/src/main/controller/GearController.java
@@ -7,9 +7,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import model.DataModel;
 import javafx.scene.control.Button;
 
 import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.binding.Bindings;
 
 import view.AlertDialog;
@@ -32,14 +35,29 @@ public class GearController extends Controller{
 	@FXML
 	private Button resetButton;
 	
+	private BooleanBinding isIpAddressFieldEmpty;
+	private BooleanBinding isPortFieldEmpty;
 	@FXML
 	public void initialize() {
-		BooleanBinding isIpAddressFieldEmpty = Bindings.isEmpty(ipAddress.textProperty());
-		BooleanBinding isPortFieldEmpty = Bindings.isEmpty(port.textProperty());
-		connectionButton.disableProperty().bind(isIpAddressFieldEmpty.or(isPortFieldEmpty));
-		resetButton.disableProperty().bind(isIpAddressFieldEmpty.and(isPortFieldEmpty));
+		
+		isIpAddressFieldEmpty = Bindings.isEmpty(ipAddress.textProperty());
+		isPortFieldEmpty = Bindings.isEmpty(port.textProperty());
+		
 	}
-	
+	@Override
+	public void init (DataModel model, Stage controlledStage) {
+		super.init(model, controlledStage);
+		if (!model.isConnected()) {
+			connectionButton.disableProperty().bind(isIpAddressFieldEmpty.or(isPortFieldEmpty));
+			resetButton.disableProperty().bind(isIpAddressFieldEmpty.and(isPortFieldEmpty));
+		}else {
+			connectionButton.setText("Disconnect");
+			ipAddress.setDisable(true);
+			port.setDisable(true);
+			resetButton.setDisable(true);
+			
+		}
+	}
 	private boolean re_ip(final String ip) {
 	    String pattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
 			     "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
@@ -54,36 +72,45 @@ public class GearController extends Controller{
 	}
 	
 	public void connectionClicked (MouseEvent event) {
-		String ipAddr = ipAddress.getText();
-		int port_conn = new Integer(port.getText()).intValue();
-		if (re_ip(ipAddr)){
-				if (port_conn>=1 && port_conn<=65535){
-					try {
-						model.connect(ipAddr, port_conn);
-						controlledStage.close();
-						System.out.println("Connessione AVVENUTA");
-					} catch (IOException e) {
-						new AlertDialog(AlertType.ERROR,
+		if (connectionButton.getText().equals("Connect")) {
+			String ipAddr = ipAddress.getText();
+			int port_conn = new Integer(port.getText()).intValue();
+			if (re_ip(ipAddr)){
+					if (port_conn>=1 && port_conn<=65535){
+						try {
+							model.connect(ipAddr, port_conn);
+							controlledStage.close();
+							System.out.println("Connessione AVVENUTA");
+						} catch (IOException e) {
+							new AlertDialog(AlertType.ERROR,
+									"CONNECTION FAILED",
+									"CONNECTION PROBLEMS",
+									"There is a problem with the server... Try to replace IP or Port!",
+									"connection_failed.png"
+									);
+						}
+					}else {
+						new AlertDialog(AlertType.WARNING,
 								"CONNECTION FAILED",
 								"CONNECTION PROBLEMS",
-								"There is a problem with the server... Try to replace IP or Port!",
-								"connection_failed.png"
+								"The Port doesn't respect the standards...Try again!"
 								);
 					}
 				}else {
-					new AlertDialog(AlertType.WARNING,
+					new AlertDialog(AlertType.ERROR,
 							"CONNECTION FAILED",
 							"CONNECTION PROBLEMS",
-							"The Port doesn't respect the standards...Try again!"
+							"The IP Address doesn't respect the standards...Try again!"
 							);
 				}
-			}else {
-				new AlertDialog(AlertType.ERROR,
-						"CONNECTION FAILED",
-						"CONNECTION PROBLEMS",
-						"The IP Address doesn't respect the standards...Try again!"
-						);
+		}else {
+			try {
+			model.disconnect();
+			controlledStage.close();
+			}catch (IOException e){
+				
 			}
+		}
 	}
 	
 }
