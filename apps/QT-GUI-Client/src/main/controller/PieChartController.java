79fbab5162d2a0cf0dbaf19151c5b67934da3e89
Davide Balice <basket-one@libero.it>
2019-11-17 00:22:11 +0100

    Added Pie Chart plotting

---
+++ apps/QT-GUI-Client/src/main/controller/PieChartController.java
@@ -0,0 +1,48 @@
+package controller;
+
+import javafx.scene.chart.PieChart;
+import javafx.scene.paint.Color;
+
+import java.util.LinkedList;
+
+import javafx.collections.ObservableList;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+
+public class PieChartController extends Controller {
+	@FXML
+	private PieChart pieChart;
+	
+	public void updateChart(LinkedList<LinkedList<LinkedList<String>>> tableData) {
+		ObservableList<PieChart.Data> pieData = FXCollections.observableArrayList();
+		LinkedList<String> names = getNames(tableData);
+		for(int i=0; i<names.size(); i++) {
+			pieData.add(new PieChart.Data(names.get(i), tableData.get(i).size()));
+		}
+		pieChart.setLegendVisible(false);
+		pieChart.setData(pieData);
+		for(int i=0; i<names.size(); i++) {
+			int id = Integer.parseInt(tableData.get(i).get(0).get(0));
+			Color background = Color.hsb(
+					(id * 67.0d) % 360.0d,
+					(0.3d + id*0.01) %1.0d,
+					0.8d);
+			pieData.get(i).getNode().setStyle("-fx-pie-color: #" + background.toString().substring(2, 8));
+		}
+		
+	}
+	
+	private LinkedList<String> getNames(LinkedList<LinkedList<LinkedList<String>>> tableData){
+		LinkedList<String> names = new LinkedList<String>();
+		for(LinkedList<LinkedList<String>> cluster: tableData) {
+			for(LinkedList<String> tuple: cluster) {
+				if(tuple.getLast().equals("0.0")) {
+					names.add(tuple.subList(0, tuple.size()-1).toString());
+					break;
+				}
+			}
+		}
+		return names;
+	}
+}
---
+++ apps/QT-GUI-Client/src/main/view/PieChartView.java
@@ -0,0 +1,22 @@
+package view;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+import model.DataModel;
+import controller.PieChartController;
+public class PieChartView extends View {
+	private static final String TITLE = "CHART VIEW";
+	
+	private static final String ICON = "mining.png";
+	
+	private static final String LAYOUT = "pieChart.fxml";
+	
+	public PieChartView(DataModel model, LinkedList<LinkedList<LinkedList<String>>> tableData) throws IOException {
+		super(model, LAYOUT, ICON, TITLE);
+		
+		getController().init(model, getStage());
+		((PieChartController)getController()).updateChart(tableData);
+		getStage().show();
+	}
+}
