c541413306566ca54fa0559604ff6bc8a6dde938
Samuele del Vescovo <delvescovo.crafter@gmail.com>
2019-11-19 23:21:48 +0100

    Added java doc comment for coloredController

--- apps/QT-GUI-Client/src/main/controller/ColoredController.java
+++ apps/QT-GUI-Client/src/main/controller/ColoredController.java
@@ -2,8 +2,17 @@ package controller;
 
 import javafx.scene.paint.Color;
 
+/**
+ * This abstract class is a controller that handles colors
+ */
+
 public abstract class ColoredController extends Controller {
 
+	/**
+	 * Generate a pastel color based on integer
+	 * @param i generic number
+	 */
+	
 	protected Color generatePastelColor(int i) {
 		return Color.hsb (
 				(i * 67.0d) % 360.0d,
