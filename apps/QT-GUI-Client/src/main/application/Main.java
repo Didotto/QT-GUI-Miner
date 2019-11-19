1490589f75e41594c25ed728416c475034e4bddc
Samuele del Vescovo <delvescovo.crafter@gmail.com>
2019-11-19 21:50:38 +0100

    Added java doc for main

--- apps/QT-GUI-Client/src/main/application/Main.java
+++ apps/QT-GUI-Client/src/main/application/Main.java
@@ -10,12 +10,19 @@ import model.DataModel;
 import view.AlertDialog;
 import view.MainView;
 
+/**
+ * This class rappresent the beginning of the software (client-side)
+ */
+
 public class Main extends Application {
 	public static void main(String args[]) {
 		launch(args);
 	}
 	
-	@Override
+	/**
+	 * Starts the client
+	 * @param primaryStage the firt stage
+	 */
 	public void start(Stage primaryStage) {
 		DataModel model = new DataModel();
 		try {
